6968d879ea9fdf7cc4732b3ffad18359264fc1c0
Rafael Arantes <rafael-fullstack@outlook.com>
2025-05-22 18:11:10 -0300

    Refatoração no cadastro de produtos

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -8,7 +8,7 @@ namespace EstoqueVendas.Models
         [Required(ErrorMessage = "Digite o nome do produto")]
         public string ProdutoNome { get; set; }
         [Required(ErrorMessage = "Informe o fornecedor")]
-        public int FornecedorId { get; set; }
+        public int? FornecedorId { get; set; }
         public virtual Fornecedor Fornecedor { get; set; }
     }
 }
